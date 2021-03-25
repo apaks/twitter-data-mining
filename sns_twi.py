d0355ab912591b59636e860571e0e992250ba430
apaks <18731254+apaks@users.noreply.github.com>
2021-03-25 16:27:25 -0400

    load tweets using snscrape

---
+++ sns_twi.py
@@ -0,0 +1,337 @@
+#author: apaks
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import snscrape.modules.twitter as sntwitter
+import twidm_functions as twidm
+
+# from bokeh.plotting import figure, ColumnDataSource, show
+# from bokeh.models import HoverTool
+import re
+# download data
+import yfinance as yf
+#plotly
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+# sns.set_context("paper")
+plt.rc("axes.spines", top=False, right=False)
+
+def get_source(source):
+    st_str = source.find(">") + 1
+    end_str = source.find("</a>")
+    return source[st_str:end_str]
+
+@st.cache
+def load_tweets(q_str, max_tweets = 100):
+
+    # Creating list to append tweet data to
+    tweets_list = []
+    
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for idx, tweet in enumerate(sntwitter.TwitterSearchScraper(q_str).get_items()):
+        if idx > max_tweets:
+            break
+        source = get_source(tweet.source)
+        text = tweet.content.lower()
+        if "Twit" in source and "away" not in text and "give" not in text:
+            tweets_list.append([tweet.date, tweet.id, text, tweet.replyCount, tweet.retweetCount,
+                                tweet.likeCount, tweet.quoteCount,  source, tweet.url,
+                tweet.user.id, tweet.user.description , tweet.user.followersCount, tweet.user.friendsCount
+                                ])
+
+    # Creating a dataframe from the tweets list above
+    df_tweets = pd.DataFrame(tweets_list, columns=['created_at', 'id', 'text',  
+                                    'replyCount', 'retweetCount', 'likeCount', 'quoteCount', "source", "tweet_url",
+                                    'user_id', "user_bio" , 'followers_count', 'friends_count' ])
+    
+    return df_tweets
+
+def plot_ohlc(fin_data):
+
+    fig = make_subplots(rows=2, cols=1, row_heights=[0.8, 0.2],shared_xaxes=True,
+                        vertical_spacing=0.02)
+    fig.add_trace(go.Ohlc(x=fin_data.index,
+            open=fin_data['Open'],
+            high=fin_data['High'],
+            low=fin_data['Low'],
+            close=fin_data['Close']), row = 1, col = 1)
+
+    fig.add_trace(go.Bar(x=fin_data.index, y=fin_data["Volume"], marker_color = "black" ), row = 2, col = 1)    
+    fig.update(layout_xaxis_rangeslider_visible=False)
+
+    fig.update_layout(
+        title=user_input.upper(),
+        plot_bgcolor="#FFFFFF",
+        hovermode="x",
+        hoverdistance=100, # Distance to show hover label of data point
+        spikedistance=-1, # Distance to show spike
+        xaxis=dict(
+    #         title="time",
+            linecolor="#BCCCDC",
+    #         showspikes=True, # Show spike line for X-axis
+            # Format spike
+            spikethickness=2,
+            spikedash="dot",
+            spikecolor="#999999",
+            spikemode="toaxis+across",
+        ),
+        yaxis=dict(
+            title="Price",
+            linecolor="#BCCCDC"
+        ),
+        yaxis2=dict(
+            title="Volume",
+            linecolor="#BCCCDC"
+        )
+    )
+    return fig
+   
+
+sian = twidm.SentimentIntensityAnalyzer()
+
+models= ["vader", "txtblob"]
+punctuation = list(twidm.string.punctuation)
+stop = twidm.stopwords.words('english') + punctuation 
+emoticons_str = r"""
+(?:
+    [:=;] # Eyes
+    [oO\-]? # Nose (optional)
+    [D\)\]\(\]/\\OpP] # Mouth
+)"""
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+    r'\n',
+    r"^\s+|\s+$"
+]
+cleaner_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+
+
+
+st.title('Twitter Trends')
+st.markdown("### Go to Navigation panel after loading tweets")
+
+sel_flag = st.radio("Are you interested in", ["stocks", "crypto"])
+user_input = st.text_input("Put a ticker name eg. TSLA or BTC)")
+
+dt_range = st.date_input("Enter dates of interest", [])
+
+# if st.button('Load tweets'):
+if user_input and len(dt_range)==2:
+    
+    q_str = f"{user_input} {sel_flag} since:{dt_range[0]} until:{dt_range[1]} lang:en"  
+    st.write(q_str)
+    max_tweets = 10000
+    data = load_tweets(q_str, max_tweets)
+    df_tweets = data.copy()
+    st.write("{} Tweets are loaded".format(df_tweets.shape[0]))
+
+    regex = re.compile('[^a-zA-Z]')
+    #First parameter is the replacement, second parameter is your input string
+    user_input = regex.sub('', user_input)
+    if sel_flag == "stocks":
+        try:
+            fin_data = yf.download(tickers = user_input, start = dt_range[0], end = dt_range[1], interval = '15m')
+            fig = plot_ohlc(fin_data)
+            st.plotly_chart(fig)
+        except:
+            st.warning("Cannot download data, did you choose stocks?")
+    else:
+        try:
+            fin_data = yf.download(tickers = user_input + "-USD", start = dt_range[0], end = dt_range[1], interval = '30m')
+            fig = plot_ohlc(fin_data)
+            st.plotly_chart(fig)
+        except:
+            st.warning("Cannot download data, did you choose crypto")
+
+
+
+else:
+    st.text("No input, type the ticker name")
+
+st.sidebar.title("Navigation")
+
+
+
+
+# page = st.sidebar.selectbox("Choose a page", ['Exploration', 'Tweets', 'Users'])
+page = st.sidebar.radio("Choose a page", ('Load tweets', 'Sentiment across time', 'Sentiment across users'))
+
+st.sidebar.title("About")
+st.sidebar.warning("Let's see whether we can predict trends in stocks/crypto market from the sentiment analysis of tweets")
+st.sidebar.info("- Retweets are not included            \n \
+- Only recent tweets are loaded        \n \
+- Seacrh limited to ~20K tweets        \n \
+Powered by [Streamlit] (https://docs.streamlit.io/en/stable/api.html), \
+    [Tweepy] (http://docs.tweepy.org/en/latest/), [yfinance](https://pypi.org/project/yfinance/)   \
+    and [Plotly](https://plotly.com/)          \n " )
+
+if page == 'Load tweets':
+    
+    if user_input and len(dt_range)==2:
+        st.write(df_tweets.head())
+        st.markdown('### Top 3 tweets')
+        
+        for idx in df_tweets.nlargest(3, "retweetCount").index:
+            st.write(df_tweets.iloc[idx].text)
+            st.write(df_tweets.iloc[idx].created_at)
+            st.write(df_tweets.iloc[idx][["retweetCount", 'likeCount' , "followers_count"]])
+            st.write(df_tweets.iloc[idx].tweet_url)
+           
+elif page == "Sentiment across time":
+    st.title('Lets analyze tweets')
+    # try:
+    d_txtblob, d_vader, count_terms, count_hash = twidm,sentiment_analysis(df_tweets)
+    df_tweets[ "txtblob"] = df_tweets["id"].map(d_txtblob)
+    df_tweets["vader"] = df_tweets["id"].map(d_vader)
+    # except:
+    #     st.write("Load some tweets first")
+
+    if st.checkbox("Activity across time"):
+        # plot tweet counts over time
+        fig = twidm.get_tweet_counts_overtime(fin_data, df_tweets["created_at"], "10T")
+        st.plotly_chart(fig)
+
+    if st.checkbox("Tweet processing and sentiment analysis"):
+  
+        st.markdown('### The most common words')
+        st.text(count_hash.most_common(100))
+        st.text(count_terms.most_common(100))
+
+        # plot wordcloud
+        _input = count_terms
+        fig = twidm.plot_wordcloud(_input)
+        st.pyplot(fig)
+
+        st.markdown('### Sentiment analysis')
+  
+        st.write(df_tweets.head())
+        
+        columns = st.multiselect(
+            label='What column to you want to display', options= ["retweetCount", 
+            "followers_count", "friends_count", "likeCount"])
+        # col = "likeCount"
+        if len(columns) > 0:
+            for col in columns:
+                st.write(col)
+                fig = twidm.sentim_by_col(col, models[0])
+                st.plotly_chart(fig)
+
+                fig = twidm.sentim_by_col(col, models[1])
+                st.plotly_chart(fig)
+
+    if st.checkbox("Sentiment analysis across time"):
+        
+        tmp =  df_tweets[["created_at", "vader", "txtblob"]].set_index("created_at").resample('1H').mean().reset_index()
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+        fig.add_trace(go.Scatter(x = fin_data.index, y=fin_data['High'],
+                    name=user_input), secondary_y = False)
+
+        fig.add_trace(go.Scatter(x=tmp['created_at'], y= tmp["vader"],
+                    name='vader'), secondary_y = True)
+
+        fig.add_trace(go.Scatter(x=tmp['created_at'], y= tmp["txtblob"],
+                    name='txtblob'), secondary_y = True)
+
+        st.plotly_chart(fig)
+
+
+else:
+    st.title('Lets look at users')
+    # st.write(df_tweets.nlargest(5, ["followers_count", "retweetCount"]))
+    st.markdown("### Users with trending tweets")
+    try:
+
+        st.write(df_tweets.nlargest(5, ["retweetCount", "followers_count"]))
+    except: 
+        st.write("load some tweets first")
+
+    if st.checkbox("Cluster users on their profile description"):
+        # print("")
+        unique_user_map = dict(zip(df_tweets.user_id.values, df_tweets.user_bio.values))
+
+        unique_user_cnt = len(unique_user_map.keys())
+        vec = TfidfVectorizer(preprocessor=replace_urls,
+                            tokenizer=my_tokenizer,
+                            stop_words=stop,
+                            max_features=unique_user_cnt//50,
+                            )
+
+        # we need to maintain the same ordering of users and bios
+        unique_users = []
+        unique_bios = []
+        for user, bio in unique_user_map.items():
+            unique_users.append(user)
+            if bio is None:
+                # special case for empty bios
+                bio = ''
+            unique_bios.append(bio)
+
+        # calculate the data matrix
+        bio_matrix = vec.fit_transform(unique_bios)
+        st.text("Input matrix")
+        st.write(pd.DataFrame(bio_matrix[200:210].todense(),
+              columns=[x for x in vec.get_feature_names()]).iloc[:,0:15])
+        
+        # run hdbscan on user bios
+        hdbs = twidm.fit_hdbscan(bio_matrix)
+        fig = plot_hdbs_clustersize(hdbs)
+        st.pyplot(fig)
+        st.markdown("#### Strongest features")
+        strongest_features(hdbs, vec, topk=15)
+
+        # fit umap
+        # embedding = fit_umap(bio_matrix.todense())
+
+        # df_bios = get_plottable_df(unique_users, unique_bios, embedding, hdbs.labels_)
+        # st.write(df_bios.head())
+
+        # st.bokeh_chart(plot_umap(df_bios.sample(5000),
+        #        'UMAP of clustered users ["(cluster #) bio"]'))
+        
+        if st.checkbox("Opinion mining across identified user groups"):
+            d_txtblob, d_vader, count_terms, count_hash = twidm.sentiment_analysis(df_tweets)
+            d_user_label = dict(zip(unique_users, hdbs.labels_))
+
+            df_tweets[ "txtblob"] = df_tweets["id"].map(d_txtblob)
+            df_tweets["vader"] = df_tweets["id"].map(d_vader)
+            df_tweets["cluster"] = df_tweets["user_id"].map(d_user_label)
+
+            st.write(df_tweets.head())
+            st.markdown("### Sentiment by groups")
+            fig, ax = plt.subplots(2, 1,  sharey = True, sharex = True)
+            for idx, m in enumerate(models):  
+                sns.violinplot(x = "cluster", y = m, data = df_tweets, ax = ax[idx])
+            st.pyplot(fig)
+        
+            if st.checkbox("Sentiment analysis across time"):
+
+                columns = st.multiselect( 
+                label='What cluster to you want to display', options= sorted(df_tweets.cluster.unique().tolist()))
+         
+                fig, ax = plt.subplots(2, 1, sharex = True, sharey = True)
+                if len(columns) > 0:
+                    for col in columns:
+
+                        tmp = df_tweets[df_tweets.cluster == col]
+                        tmp =  tmp[["created_at", "cluster", models[0], models[1]]].set_index("created_at").resample('1H').mean().reset_index()
+                        sns.lineplot(x = "created_at" , y = models[0],  data = tmp, ci = False , ax = ax[0], label = col )
+                        sns.lineplot(x = "created_at" , y = models[1],  data = tmp, ci = False , ax = ax[1] )
+
+                        date_form = DateFormatter("%m-%d")
+                        ax[1].xaxis.set_major_formatter(date_form)
+                        ax[1].set_xlabel('Date')
+                        
+                        # plt.legend(bbox_to_anchor=(.7, 1), loc=2, borderaxespad=0.)
+                        plt.xticks(rotation = 45)
+                        plt.tight_layout()
+                    st.pyplot(fig)
+           
---
+++ twidm_functions.py
@@ -0,0 +1,294 @@
+#@author apaks
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+import re
+import operator 
+import string
+from collections import Counter
+
+from wordcloud import WordCloud
+from matplotlib.dates import DateFormatter
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+# nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+from textblob import TextBlob
+# cluster users
+from sklearn.feature_extraction.text import TfidfVectorizer
+import hdbscan
+
+
+def get_tweet_counts_overtime(fin_data, ts, res_window = "1H"):
+    
+    tmp = ts
+    ones = [1]*len(tmp)
+    tmp = pd.DataFrame(ones, index = tmp)
+    tmp = tmp.resample(res_window).sum().fillna(0).reset_index()
+    tmp.columns = ['date', 'freq']
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(go.Scatter(x=fin_data.index, y=fin_data['High'],
+            ), secondary_y = False)
+
+    fig.add_trace(go.Scatter(x=tmp["date"], y=tmp["freq"], 
+            name = "count of tweets"), secondary_y = True)
+
+    fig.update_layout(
+        # title=user_input.upper(),
+        plot_bgcolor="#FFFFFF",
+        hovermode="x",
+        hoverdistance=100, # Distance to show hover label of data point
+        spikedistance=-1, # Distance to show spike
+        xaxis=dict(
+    #         title="time",
+            linecolor="#BCCCDC",
+    #         showspikes=True, # Show spike line for X-axis
+            # Format spike
+            spikethickness=2,
+            spikedash="dot",
+            spikecolor="#999999",
+            spikemode="toaxis+across",
+        ),
+        yaxis=dict(
+            title="Price",
+            linecolor="#BCCCDC"
+        ),
+        yaxis2=dict(
+            title="Volume",
+            linecolor="#BCCCDC"
+        )
+    )
+    return fig
+
+
+def plot_wordcloud(_input):
+    wc = WordCloud( contour_width=3, contour_color= 'steelblue',
+                    background_color ='white', max_font_size=50, 
+                    max_words=200, random_state=42, 
+                    min_font_size = 10).generate_from_frequencies(_input) 
+    
+    # plot the WordCloud image     
+    fig, ax = plt.subplots(figsize = (8, 8), facecolor = None)                   
+    ax.imshow(wc, interpolation='bilinear') 
+    plt.axis("off") 
+    plt.tight_layout(pad = 0) 
+    return fig
+
+
+def sentim_by_col(col, y):
+    if "fol" in col or "fri" in col:
+        bins = [-1, 5000, 10000, 50000, 100000, 2e8]
+        bin_labels = ["<5000", "<10000", "<50000", "less than 100000", "more than 100000"]
+    else:
+        bins = [-1, 50, 100, 500, 1000, 2e7]
+        bin_labels = ["<50", "<100", "<500", "less than 1000", "more than 1000"]
+    tmp = pd.cut(df_tweets[col], bins = bins,  labels= bin_labels) 
+    df_tweets[col + "_cut"] = tmp
+    
+    subfig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig = px.scatter(df_tweets, x="created_at", y= y, size= col, 
+                hover_name="user_bio",
+                    color= col + '_cut',
+                    log_x=False, size_max=40)
+    fig2 = px.line(x=fin_data.index, y=fin_data['High'])
+    for tr in fig.data:
+        subfig.add_trace(tr, secondary_y = True)
+    for tr in fig2.data:
+        subfig.add_trace(tr, secondary_y = False)
+    subfig['layout']["yaxis"]["title"] = f"Sentiment algo: {y}"
+    subfig['layout']["legend"]["title"] = col
+
+    return subfig
+
+@st.cache
+def sentiment_analysis(df_tweets):
+    tweet_tokenizer = TweetTokenizer()
+    d_txtblob = {}
+    d_vader = {}
+
+    count_terms = Counter()
+    count_hash = Counter()
+    # count terms and perform sentiment analysis
+    for idx in df_tweets.index[:]:
+        text = df_tweets.iloc[idx].text.lower()
+        if len(text) < 5:
+            continue
+        cleaned_text = process_tweet(text)
+        tweet_id = df_tweets.iloc[idx].id 
+        
+        # word freqeuncy
+        terms_hash = [term for term in tweet_tokenizer.tokenize(text) 
+                if term not in stop and len(term) > 3 and term.startswith('#')]
+        terms_only = [term for term in tweet_tokenizer.tokenize(text) 
+                if term not in stop and len(term) > 3 and
+                not term.startswith(('#', '@'))] 
+        
+        # Update the counter
+        count_hash.update(terms_hash)
+        count_terms.update(terms_only)
+        
+        # sentiment analysis
+        try:
+            compound = sian.polarity_scores(cleaned_text)["compound"]
+            d_vader[tweet_id] = round(compound, 5)
+
+            txtblob = TextBlob(cleaned_text).sentiment[0]
+            d_txtblob[tweet_id] = round(txtblob, 5)
+        
+        except Exception as e: 
+            print (str(e))
+
+    return d_txtblob, d_vader, count_terms, count_hash
+
+
+
+
+
+
+def process_tweet(tweet): 
+    return cleaner_re.sub("", tweet)
+
+
+
+def replace_urls(in_string, replacement=None):
+    """Replace URLs in strings. See also: ``bit.ly/PyURLre``
+
+    Args:
+        in_string (str): string to filter
+        replacement (str or None): replacment text. defaults to '<-URL->'
+
+    Returns:
+        str
+    """
+    replacement = '<-URL->' if replacement is None else replacement
+    pattern = re.compile('(https?://)?(\w*[.]\w+)+([/?=&]+\w+)*')
+    return re.sub(pattern, replacement, in_string)
+
+def my_tokenizer(in_string):
+    """
+    Convert `in_string` of text to a list of tokens using NLTK's TweetTokenizer
+    """
+    # reasonable, but adjustable tokenizer settings
+    tokenizer = TweetTokenizer(preserve_case=False,
+                               reduce_len=True,
+                               strip_handles=True)
+    tokens = tokenizer.tokenize(in_string)
+    tokens = [word for word in tokens if len(word) > 2 and "#" not in word]
+    return tokens
+
+@st.cache
+def fit_hdbscan(data):
+
+    hdbs = hdbscan.HDBSCAN(min_cluster_size=100,
+                        prediction_data=True,
+                        core_dist_n_jobs=-1,
+                        memory='data')
+    hdbs.fit(data.todense())
+    return hdbs
+
+def plot_hdbs_clustersize(hdbs):
+    # get the population sizes
+    label_counts = Counter(hdbs.labels_)
+    xs, ys = [], []
+    for k,v in label_counts.items():
+        xs.append(k)
+        ys.append(v)
+    fig, ax = plt.subplots()
+    # draw the chart
+    ax.bar(xs, ys)
+
+    plt.xticks(range(-1, len(label_counts)))
+    plt.ylabel('population')
+    plt.xlabel('cluster label')
+    plt.title('population sizes ({} clusters found by hdbscan)'.format(len(label_counts) - 1));
+    return fig
+
+def strongest_features(model, vectorizer, topk=10):
+    """
+    Helper function to display a simple text representation of the top-k most
+    important features in our fit model and vectorizer.
+
+    model: sklearn model
+    vectorizer: sklearn vectorizer
+    topk: k numbers of words to get per cluster
+
+    """
+    # these parts are model-independent
+
+    features = vectorizer.get_feature_names()
+    relevant_labels = [ x for x in set(model.labels_) if x >= 0 ]
+    # -1 is a noise cluster
+    for this_label in relevant_labels:
+        matching_rows = np.where(hdbs.labels_ == this_label)[0]
+        coeff_sums = np.sum(bio_matrix[matching_rows], axis=0).A1
+        sorted_coeff_idxs = np.argsort(coeff_sums)[::-1]
+        st.text('Cluster {}: '.format(this_label))
+        tmp = " "
+        for idx in sorted_coeff_idxs[:topk]:
+            tmp = tmp + features[idx] + " "
+        st.text(tmp)
+        # st.text("")
+
+def get_plottable_df(users, bios, two_d_coords, labels):
+    """
+    Combine the necessary pieces of data to create a data structure that plays
+    nicely with the our 2d tsne chart.
+
+    Note: assumes that all argument data series
+    are in the same order e.g. the first user, bio, coords, and label
+    all correspond to the same user.
+    """
+    # set up color palette
+    num_labels = len(set(labels))
+    colors = sns.color_palette('hls', num_labels).as_hex()
+    color_lookup = {v:k for k,v in zip(colors, set(labels))}
+    # combine data into a single df
+    df = pd.DataFrame({'uid': users,
+                       'text': bios,
+                       'label': labels,
+                       'x_val': two_d_coords[:,0],
+                       'y_val': two_d_coords[:,1],
+                      })
+    # convert labels to colors
+    df['color'] = list(map(lambda x: color_lookup[x], labels))
+    return df
+
+@st.cache
+def fit_umap(data):
+    reducer = umap.UMAP()
+    embedding = reducer.fit_transform(data)
+    return embedding
+
+def plot_umap(df, title='Umap plot'):
+    # add our DataFrame as a ColumnDataSource for Bokeh
+    plot_data = ColumnDataSource(df)
+    # configure the chart
+    umap_plot = figure(title=title, plot_width=800, plot_height=700, tools=('pan, box_zoom, reset'))
+    # add a hover tool to display words on roll-over
+    umap_plot.add_tools(
+        HoverTool(tooltips = """<div style="width: 400px;">(@label) @text</div>""")
+    )
+    # draw the words as circles on the plot
+    umap_plot.circle('x_val', 'y_val',
+                     source=plot_data,
+                     color='color',
+                     line_alpha=0.2,
+                     fill_alpha=0.1,
+                     size=7,
+                     hover_line_color='black')
+    # configure visual elements of the plot
+    umap_plot.title.text_font_size = '12pt'
+    umap_plot.xaxis.visible = False
+    umap_plot.yaxis.visible = False
+    umap_plot.grid.grid_line_color = None
+    umap_plot.outline_line_color = None
+    return umap_plot
+
